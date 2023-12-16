c99857fb8f0a4385ee102d5f17e538efa321c750
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-12-17 00:51:33 +0530

    [SpringSecurity]-#5- Configure users from JDBC for accessing the APIs

---
+++ springSecurity/src/main/resources/sql/scripts_1.sql
@@ -0,0 +1,32 @@
+CREATE database gautybank;
+
+use gautybank;
+
+CREATE TABLE `users`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(45) NOT NULL,
+	`password` VARCHAR(45) NOT NULL,
+	`enabled` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `authorities`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(45) NOT NULL,
+	`authority` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+INSERT IGNORE INTO `users` VALUES (NULL,'happy','12345','1');
+INSERT IGNORE INTO `authorities` VALUES (NULL,'happy','write');
+
+CREATE TABLE `customer`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`email` VARCHAR(45) NOT NULL,
+	`pwd` VARCHAR(45) NOT NULL,
+	`role` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO `customer` (`email`,`pwd`,`role`) VALUES ('ted@gmail.com','123','admin');
