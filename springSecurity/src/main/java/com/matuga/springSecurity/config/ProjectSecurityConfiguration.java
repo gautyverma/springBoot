dcb6a7813ed728f73e159a45106e4718de03160f
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-12-18 13:57:55 +0530

    [SpringSecurity]-#10-CORS Impl for UI to Backend API access

--- springSecurity/src/main/java/com/matuga/springSecurity/config/ProjectSecurityConfiguration.java
+++ springSecurity/src/main/java/com/matuga/springSecurity/config/ProjectSecurityConfiguration.java
@@ -1,13 +1,16 @@
 package com.matuga.springSecurity.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class ProjectSecurityConfiguration {
@@ -17,7 +20,22 @@ public class ProjectSecurityConfiguration {
      * Below are the custom security configuration
      * */
 
-    http.csrf()
+    http.cors()
+        .configurationSource(
+            new CorsConfigurationSource() {
+              @Override
+              public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L);
+                return config;
+              }
+            })
+        .and()
+        .csrf()
         .disable()
         .authorizeHttpRequests(
             (requests) ->
