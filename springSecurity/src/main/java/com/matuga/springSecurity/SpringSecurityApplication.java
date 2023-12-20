7917c3fc4a865280d79f77420829a6dc85292974
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-12-20 23:37:40 +0530

    [SpringSecurity]-#13- FilterChain_1 Logging and enable debug = true - NOT FOR PRODUCTION

--- springSecurity/src/main/java/com/matuga/springSecurity/SpringSecurityApplication.java
+++ springSecurity/src/main/java/com/matuga/springSecurity/SpringSecurityApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.matuga.springSecurity.repository")
 @EntityScan("com.matuga.springSecurity.model")
+@EnableWebSecurity(debug = true)
 public class SpringSecurityApplication {
 
   public static void main(String[] args) {
