c67184222d33d707742366c07e51fed4c46e6e59
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-12-24 00:23:24 +0530

    [SpringSecurity]-#19- @PostAuthorize method Impl

--- springSecurity/src/main/java/com/matuga/springSecurity/controller/LoansController.java
+++ springSecurity/src/main/java/com/matuga/springSecurity/controller/LoansController.java
@@ -4,6 +4,7 @@ import com.matuga.springSecurity.model.Loans;
 import com.matuga.springSecurity.repository.LoanRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class LoansController {
   @Autowired private LoanRepository loanRepository;
 
   @GetMapping("/myLoans")
+  @PostAuthorize("hasRole('ROOT')")
   public List<Loans> getLoanDetails(@RequestParam int id) {
     List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
     if (loans != null) {
