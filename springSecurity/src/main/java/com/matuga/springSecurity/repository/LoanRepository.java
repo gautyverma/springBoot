a0a63eb585c899dbe5f0c78779292dd56c31bd86
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-12-24 00:09:31 +0530

    [SpringSecurity]-#18- @PreAuthorize method Impl

--- springSecurity/src/main/java/com/matuga/springSecurity/repository/LoanRepository.java
+++ springSecurity/src/main/java/com/matuga/springSecurity/repository/LoanRepository.java
@@ -3,10 +3,12 @@ package com.matuga.springSecurity.repository;
 import com.matuga.springSecurity.model.Loans;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
 
+  @PreAuthorize("hasRole('ADMIN')")
   List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 }
