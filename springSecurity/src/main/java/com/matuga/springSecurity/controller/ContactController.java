e04a7971b76fdbfaaf50c2bb5588935d0cea2acf
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-12-24 00:33:17 +0530

    [SpringSecurity]-#19- Method Level Security-@PostFilter

--- springSecurity/src/main/java/com/matuga/springSecurity/controller/ContactController.java
+++ springSecurity/src/main/java/com/matuga/springSecurity/controller/ContactController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreFilter;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,8 @@ public class ContactController {
   @Autowired private ContactRepository contactRepository;
 
   @PostMapping("/contact")
-  @PreFilter("filterObject.contactName != 'Test'")
+//  @PreFilter("filterObject.contactName != 'Test'")
+  @PostFilter("filterObject.contactName != 'Test'")
   public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
     /*
         contact.setContactId(getServiceReqNumber());
