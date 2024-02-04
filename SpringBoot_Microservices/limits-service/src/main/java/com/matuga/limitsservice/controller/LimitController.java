8cb9e3b861c69806f1b3f8ee5aeede0073715690
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-02-04 21:39:29 +0530

    [Spring_Microservice]-#2 - Get configuration values from application.properties for Limits Service

--- SpringBoot_Microservices/limits-service/src/main/java/com/matuga/limitsservice/controller/LimitController.java
+++ SpringBoot_Microservices/limits-service/src/main/java/com/matuga/limitsservice/controller/LimitController.java
@@ -1,14 +1,21 @@
 package com.matuga.limitsservice.controller;
 
+import com.matuga.limitsservice.configuration.Configuration;
 import com.matuga.limitsservice.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitController {
 
+  @Autowired private Configuration configuration;
+
   @GetMapping("/limits")
   public Limits retrieveLimit() {
-    return new Limits(1, 1000);
+    //    return new Limits(1, 1000);
+    return new Limits(
+        configuration.getMinimum(),
+        configuration.getMaximum()); // Get Value from application.properties
   }
 }
