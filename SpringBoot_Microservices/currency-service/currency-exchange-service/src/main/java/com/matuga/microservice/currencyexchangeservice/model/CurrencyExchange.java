1f6ae185c03697ae341cc997e90e341d7d6596d0
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-02-05 00:25:10 +0530

    [Spring_Microservice]-#11 - Configure JPA & initialized data on h2 DB currency-exchange microservice

--- SpringBoot_Microservices/currency-service/currency-exchange-service/src/main/java/com/matuga/microservice/currencyexchangeservice/model/CurrencyExchange.java
+++ SpringBoot_Microservices/currency-service/currency-exchange-service/src/main/java/com/matuga/microservice/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,11 +1,21 @@
 package com.matuga.microservice.currencyexchangeservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity(name = "currency_exchange")
 public class CurrencyExchange {
-  private Long id;
+  @Id @GeneratedValue private Long id;
+
+  @Column(name = "currency_from")
   private String from;
+
+  @Column(name = "currency_to")
   private String to;
+
   private BigDecimal conversionMultiple;
   private String envPort;
 
---
+++ SpringBoot_Microservices/currency-service/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,env_port)
+values(10001,'USD','INR',80,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,env_port)
+values(10002,'AUD','INR',54,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,env_port)
+values(10003,'EURO','INR',102,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,env_port)
+values(10004,'CND','INR',64,'');
