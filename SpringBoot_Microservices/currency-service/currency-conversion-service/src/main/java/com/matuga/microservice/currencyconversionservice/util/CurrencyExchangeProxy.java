5f77c4580c6adb0c3ff56c34bb83f0fd3a7e8d4d
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-02-06 00:11:38 +0530

    [Spring_Microservice]-#18 - Load Balancing the currency-exchange using eureka naming server/registry

--- SpringBoot_Microservices/currency-service/currency-conversion-service/src/main/java/com/matuga/microservice/currencyconversionservice/util/CurrencyExchangeProxy.java
+++ SpringBoot_Microservices/currency-service/currency-conversion-service/src/main/java/com/matuga/microservice/currencyconversionservice/util/CurrencyExchangeProxy.java
@@ -5,12 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/*
 @FeignClient(
     name = "currency-exchange",
     url = "localhost:8000") // mostly we give the name of the service
+*/
+@FeignClient(name = "currency-exchange") // this will call the naming-server to get up instance
 public interface CurrencyExchangeProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversion retrieveExchangeValue(
       @PathVariable String from, @PathVariable String to);
-
 }
