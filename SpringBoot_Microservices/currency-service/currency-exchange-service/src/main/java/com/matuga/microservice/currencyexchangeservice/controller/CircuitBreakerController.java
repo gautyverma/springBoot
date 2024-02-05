35fb1bec5d91d94c45d5143c704a2fec896e7f74
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-02-06 02:07:23 +0530

    [Spring_Microservice]-#22 - Base setup for circuit break Resilience4j

---
+++ SpringBoot_Microservices/currency-service/currency-exchange-service/src/main/java/com/matuga/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,12 @@
+package com.matuga.microservice.currencyexchangeservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+  @GetMapping("/sample-api")
+  public String sampleApi() {
+    return "Sample-API";
+  }
+}
