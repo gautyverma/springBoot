1a26f2c87f8aafb4d0afb577e1473069594a9c9e
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-02-22 22:56:58 +0530

    [SpringBoot]- Code Refactor

---
+++ SpringBoot_Security/JavaBrains/javaBrains-spring-security/src/main/java/com/matuga/javaBrainsspringsecurity/JavaBrainsSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.matuga.javaBrainsspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsSpringSecurityApplication.class, args);
+	}
+
+}
